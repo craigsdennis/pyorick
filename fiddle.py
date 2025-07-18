ccf85cfaceed73a51424d4d489051a2acac54e79
Craig Dennis <craigsdennis@gmail.com>
2025-07-18 00:46:51 -0700

    YOLObots assemble

---
+++ fiddle.py
@@ -0,0 +1,14 @@
+import threading
+
+
+# Installed in the main packages on the uHandPi it's in common_sdk folder
+from common.action_group_controller import ActionGroupController
+from common.ros_robot_controller_sdk import Board 
+
+def run_action_group(actNum):
+    print(f"Running action group {actNum}")
+    threading.Thread(target=agc.runAction, args=(actNum,)).start()
+
+if __name__ == "__main__":
+    board = Board()
+    agc = ActionGroupController(board)
